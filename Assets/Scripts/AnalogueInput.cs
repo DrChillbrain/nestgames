43bc5510fbd3a83bbbe53a0fb429779afaf1c879
AidenPearson25 <pearsoa6@miamioh.edu>
2024-06-20 15:09:30 +0100

    Made AnalogueInputClass

---
+++ Assets/Scripts/AnalogueInput.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AnalogueInput
+{
+    public static float getValue()
+    {
+        return 0.0f;
+    }
+}
